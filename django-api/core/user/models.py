2dbdee430a2343ad9b9b044ace2af7732d10feff
MK94 <mklecodeur@gmail.com>
2023-06-24 22:04:35 +0200

    added router

--- django-api/core/user/models.py
+++ django-api/core/user/models.py
@@ -57,6 +57,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+    
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True)
+    
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
      
