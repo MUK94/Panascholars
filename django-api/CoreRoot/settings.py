4a33dba91dffb10d988665c93b49d7977b93ec93
MK94 <mklecodeur@gmail.com>
2023-06-27 01:06:00 +0200

    installed RFsimplejwt

--- django-api/CoreRoot/settings.py
+++ django-api/CoreRoot/settings.py
@@ -40,8 +40,16 @@ INSTALLED_APPS = [
     'core',
     'core.user',
     'rest_framework',
+    'rest_framework_simplejwt',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
